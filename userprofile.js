da0d0b20a5a47d36813ad10e20ac4cb20fe7f84b
Muhammad Ali <alikhancss27@gmail.com>
2025-02-07 03:30:05 +0500

    changes in ui

--- userprofile.js
+++ userprofile.js
@@ -28,50 +28,62 @@ async function loadPosts(){
               if(currentUser.userId === myId.uid){
                 if (post_container) {
                   post_container.innerHTML += ` 
-              <div class="border border container-fluid w-75 my-3 py-1 rounded-3">
+              <div class="  container w-75 my-5 py-1 rounded-3">
                 <div class="d-flex justify-content-between">
                     <div class="d-flex flex-column rounded-circle">
                        <div class="d-flex gap-2">
-                        <img class="rounded-circle" style="width:50px; height:50px;" src=${posts.imageUrl} alt="">
+           ${usersData.imageUrl? `<img class="rounded-circle" style="width:50px; height:50px;" src=${user.imageUrl} alt=""></img>`:
+    '<img class="rounded-circle" style="width:50px; height:50px;" src="assets/istockphoto-1327592449-612x612.jpg" alt=""></img>'
+           }            
                         <p class="mt-2">${new Date(posts.created_at).toLocaleString() }<p>
                        </div>
                         <h5 class="mt-2">${currentUser.name}</h5>
                         
                     </div>
     
-    ${currentUser.userId === myId.uid?`<div class="mt-2"><button>delete</button>
+    ${currentUser.userId === myId.uid?`<div class="mt-4"><button onclick=deleteMyPost(${posts.id}) class= " btn btn-danger text-light"><i class="fa-solid fa-bucket"></i></button>
       </div>`:""}
                     
                 </div>
                 <hr>
-                <div class="row">
+                <div class="row px-2">
                 ${posts.imageUrl!==null ? `<div class="col-12 col-md-6 h-75">
                         <div class=" d-flex justify-content-center">
                             <img class="w-100 h-100  rounded-4" src=${posts.imageUrl} alt="">
                         </div>
                     </div>
-                    <div class="col-12 col-md-6 text-center text-content">
+                    <div class="col-12 mt-3 mt-md-0 col-md-6 text-center text-content rounded-4 p-2">
                         <h3>RECIPE NAME</h3>
+                         <hr>
                          <p>${posts.name}</p>
-                        <h4>INGREDIENTS</h4>
+                          <hr>
+                        <h3>INGREDIENTS</h3>
+                         <hr>
                         <p>${posts.ingredients}</p>
-                        <h4>METHOD</h4>
+                         <hr>
+                        <h3>METHOD</h3>
+                         <hr>
                         <p>${posts.methods}</p>
                     </div>`
                     :
-                    `<div class="col-12 text-center text-content">
+                    `<div class="col-12  text-center text-content rounded-4 p-2">
                         <h3>RECIPE NAME</h3>
+                         <hr>
                          <p>${posts.name}</p>
-                        <h4>INGREDIENTS</h4>
+                          <hr>
+                        <h3>INGREDIENTS</h3>
+                         <hr>
                         <p>${posts.ingredients}</p>
-                        <h4>METHOD</h4>
-                        <p>${posts.method}</p>
+                         <hr>
+                        <h3>METHOD</h3>
+                         <hr>
+                        <p>${posts.methods}</p>
                     </div>` }
                    
                     
                 </div>
                 <div class="d-flex mt-3 mb-1 justify-content-center">
-                    <div><button class=" btn btn-ouline-dark">favourites</button>
+                    <div><button class="btn btn-danger border border-light" >❤ Add to Favorites</button>
     
                     </div>
                 </div
